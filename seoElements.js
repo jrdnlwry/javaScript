3f743bd9db9f7ac8d5ea5b73b1626e7887d55c1e
jrdnlwry <113599853+jrdnlwry@users.noreply.github.com>
2022-09-29 17:30:46 -0400

    Update seoElements.js

--- seoElements.js
+++ seoElements.js
@@ -1,18 +1,18 @@
-// all internal links
+// highlight all internal links on a page green
 
 var href = document.getElementsByTagName('a');
 for(var i =0; i < href.length; i ++){
     href[i].style.backgroundColor = 'green'
 }
 
-// all H1 Tags
+// highlight all H1 Tags on a page orange
 
 var heading = document.getElementsByTagName('h1');
 for(var h = 0; h < heading.length; h++){
     heading[h].style.backgroundColor = 'orange';
 }
 
-// P Tags
+// highlight all paragraph Tags purple
 
 var paragraph = document.getElementsByTagName('p');
 for(var p = 0; paragraph.length; p++){
